c0e3ac32ad715abfa8c6a086d853f95213696763
ikmsh <mrsopelka@gmail.com>
2024-09-06 09:22:43 +0500

    изменили первое

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Zadacha5();
+    }
+    static void Zadacha5()
+    {
+        double x = 2;
+        double summa = 0;
+        for (double i = 1; i <= 10; i++)
+        {
+            summa += Math.Pow(x, i) / (x + 1);
+        }
+        Console.WriteLine(summa);
+    }
+}
