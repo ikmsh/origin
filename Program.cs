fd99d3dbc16f0bf71ba5042c4947e2f47b7e06ee
ikmsh <mrsopelka@gmail.com>
2024-09-12 09:26:32 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double step = 1;
+        double start = -10;
+        double end = 10;
+
+        Console.WriteLine($"{ "a",-10} { "a^2(a)",-10} { "Sqrt a (a)",-10}");
+
+        for (double a = start; a <= end; a += step)
+        {
+            double x = a * a; 
+            double y = Math.Sqrt(a); 
+            Console.WriteLine($"{ a,-10:F2} { x,-10:F2} { y,-10:F2}");
+        }
+    }
+}
